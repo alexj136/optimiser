5148120c64ce9090c39751dc3cac0b9c63f7522e
Alex Jeffery <apjeffery136@googlemail.com>
2015-06-17 14:16:13 +0100

    Worked on revamping CLI

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,10 @@
 module Main where
 
+import Data.Either
+import Data.List (intersperse)
 import qualified Data.Map    as M
 import System.Environment (getArgs)
+import System.Exit
 import Util
 import Lexer
 import Parser
@@ -10,20 +13,93 @@ import qualified Interpreter as I
 import qualified DataFlow    as Df
 import qualified ConstProp   as Cp
 
-main :: IO ()
+main :: IO ExitCode
 main = do
-    args <- getArgs
-    if length args == 1 then
-        do {
-            fileText <- readFile (args !! 0);
-            let tokens = scan fileText
-                linearAST = parse tokens
-                graphAST  = linearToGraph linearAST
-            in
-            putStrLn $ concat [
-                I.ppInterpretResult (I.interpret graphAST M.empty),
-                "\n\n",
-                Df.ppDataFlowResult (Df.genDataFlowInfo graphAST)]
-        }
-    else
-        putStrLn "Illegal argument(s)"
+    args        <- getArgs
+    parseResult <- return $ argParse args
+    resultStr   <- case parseResult of
+        Right actionAndFileName         -> doAction actionAndFileName
+        Left InvalidCmdSyntax           -> return helpString
+        Left (FileNotOpenable fileName) ->
+            return $ "File '" ++ fileName ++ "' could not be opened."
+    return $ case parseResult of
+        Left  _ -> ExitFailure 1
+        Right _ -> ExitSuccess
+
+-- The things that Main can do
+data Action
+    = RunOptimised
+    | RunUnoptimised
+    | ShowOptimise
+    deriving Eq
+
+data CmdError
+    = FileNotOpenable String
+    | InvalidCmdSyntax
+    deriving Eq
+
+type CmdLineResult = Either CmdError (Action, String)
+
+getProg :: IO String -> IO GraphProg
+getProg iOFileName = do
+    fileName <- iOFileName
+    fileText <- readFile fileName
+    return $ linearToGraph $ parse $ scan fileText
+
+doAction :: (Action, String) -> IO String
+doAction (action, fileText) = case action of
+    RunOptimised   -> notImplemented
+    RunUnoptimised -> notImplemented
+    ShowOptimise   -> notImplemented
+
+argParse :: [String] -> CmdLineResult
+argParse args = case args of
+
+    [fileName          ] -> Right (RunOptimised, fileName)
+    [fileName, "--run" ] -> Right (RunOptimised, fileName)
+    [fileName, "-r"    ] -> Right (RunOptimised, fileName)
+    ["--run" , fileName] -> Right (RunOptimised, fileName)
+    ["-r"    , fileName] -> Right (RunOptimised, fileName)
+
+    [fileName           , "--run-unoptimised"] ->
+        Right (RunUnoptimised, fileName)
+    [fileName           , "-u"               ] ->
+        Right (RunUnoptimised, fileName)
+    ["--run-unoptimised", fileName           ] ->
+        Right (RunUnoptimised, fileName)
+    ["-u"               , fileName           ] ->
+        Right (RunUnoptimised, fileName)
+
+    [fileName         , "--show-optimise"] -> Right (ShowOptimise, fileName)
+    [fileName         , "-s"             ] -> Right (ShowOptimise, fileName)
+    ["--show-optimise", fileName         ] -> Right (ShowOptimise, fileName)
+    ["-s"             , fileName         ] -> Right (ShowOptimise, fileName)
+
+    _ -> Left InvalidCmdSyntax
+
+helpString :: String
+helpString = concat $ intersperse "\n" $
+    [ "----------------------------------------------------------"
+    , "| COMMAND ARGUMENTS          | ACTION                    |"
+    , "----------------------------------------------------------"
+    , "| filename                   |                           |"
+    , "| filename --run             | Run 'filename'            |"
+    , "| filename -r                | with optimsiation enabled |"
+    , "| --run filename             |                           |"
+    , "| -r filename                |                           |"
+    , "----------------------------------------------------------"
+    , "| filename --run-unoptimised |                           |"
+    , "| filename -u                | Run 'filename'            |"
+    , "| --run-unoptimised filename | with no optimisation      |"
+    , "| -u filename                |                           |"
+    , "----------------------------------------------------------"
+    , "| filename --show-optimise   |                           |"
+    , "| filename -s                | Display an optimised      |"
+    , "| --show-optimise filename   | version of 'filename'     |"
+    , "| -s filename                |                           |"
+    , "----------------------------------------------------------"
+    , "| -h                         |                           |"
+    , "| --help                     | Print this message        |"
+    , "| (anything else)            |                           |"
+    , "----------------------------------------------------------"
+    ]
